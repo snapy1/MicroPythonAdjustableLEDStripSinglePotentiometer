b1b4a944f2e175f3405d45e6407889e582ef4b6b
snapy1 <snapatya@gmail.com>
2023-10-24 09:59:52 -0400

    comment

--- Settings/Brightness.py
+++ Settings/Brightness.py
@@ -9,14 +9,14 @@ class Brightness:
         if np != neopixel.NeoPixel:
             return
 
+    # Set all LEDs to a color scaled by a brightness factor.
     def set_brightness(self, color, brightness_factor):
-        """Set all LEDs to a color scaled by a brightness factor."""
         scaled_color = self.__scale_color(color, brightness_factor)
         for i in range(60):
             self.np[i] = scaled_color
         self.np.write()
 
+    # Scale a color by a brightness factor.
     def __scale_color(self, color, brightness_factor):
-        """Scale a color by a brightness factor."""
         r, g, b = color
         return (int(r * brightness_factor), int(g * brightness_factor), int(b * brightness_factor))
--- Settings/Potentiometer.py
+++ Settings/Potentiometer.py
@@ -1,5 +1,4 @@
 from machine import ADC
-from time import sleep
 
 
 class Potentiometer:
