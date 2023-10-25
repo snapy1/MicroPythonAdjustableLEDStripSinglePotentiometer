cd95d7ea4a44af37ae9e310f3f3ebaf54972e712
snapy1 <snapatya@gmail.com>
2023-10-25 10:33:43 -0400

    Removed unnecessary comment

--- main.py
+++ main.py
@@ -11,5 +11,4 @@ while True:
     if p.getPotValue() != currentPotValue:  # if the updated value is different than the old one
         # then updating currentPotValue
         currentPotValue = p.getPotValue()
-        # l.setValueWithPercentage(currentPotValue)
         l.gradientWithPercentage(currentPotValue)
